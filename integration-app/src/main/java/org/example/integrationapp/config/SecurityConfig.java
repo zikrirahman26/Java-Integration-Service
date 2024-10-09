6f17765046dbb41cacc172eebd5e4ed1658a2874
Zikri Rahman <zikrirahman16@gmail.com>
2024-10-09 22:39:24 +0700

    Spring Security

---
+++ integration-app/src/main/java/org/example/integrationapp/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.example.integrationapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/rest/api/channel/**").hasRole("channel")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic();
+        return http.build();
+    }
+}
